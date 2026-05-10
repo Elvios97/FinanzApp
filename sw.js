6dd0122e3a1793f709eb1a95681f82ec678354f3
Elvios97 <schrammeck97@gmail.com>
2026-05-10 16:12:54 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "meingeld-v2";
-const ASSETS = ["./index.html", "./manifest.json"];
+const CACHE = "meingeld-v3";
+const ASSETS = ["./index.html", "./style.css", "./app.js", "./db.js", "./manifest.json"];
 
 self.addEventListener("install", e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
@@ -15,10 +15,12 @@ self.addEventListener("activate", e => {
 
 self.addEventListener("fetch", e => {
   e.respondWith(
-    caches.match(e.request).then(cached => cached || fetch(e.request).then(resp => {
-      const clone = resp.clone();
-      caches.open(CACHE).then(c => c.put(e.request, clone));
-      return resp;
-    }))
+    caches.match(e.request).then(cached =>
+      cached || fetch(e.request).then(resp => {
+        const clone = resp.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return resp;
+      })
+    )
   );
 });
